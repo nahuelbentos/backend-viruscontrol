922bc7448789e85fb5c7efc3cc080c90c7d423d5
javier <javierms17@gmail.com>
2020-05-12 23:00:15 -0300

    clases examen departamento y fuente de datos

---
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/entities/Examen.java
@@ -0,0 +1,49 @@
+package uy.viruscontrol.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Examen")
+public class Examen {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	@ManyToOne
+	private Enfermedad enfermedad;
+	@Column
+	private EstadoExamen estado;
+	
+	
+	
+	public EstadoExamen getEstado() {
+		return estado;
+	}
+	public void setEstado(EstadoExamen estado) {
+		this.estado = estado;
+	}
+	public Examen() {
+		super();
+	}
+	public Examen(Enfermedad enfermedad) {
+		super();
+		this.enfermedad = enfermedad;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Enfermedad getEnfermedad() {
+		return enfermedad;
+	}
+	public void setEnfermedad(Enfermedad enfermedad) {
+		this.enfermedad = enfermedad;
+	}
+}
---
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/entities/FuenteDeDatos.java
@@ -0,0 +1,55 @@
+package uy.viruscontrol.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="fuente_de_datos")
+public class FuenteDeDatos {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	@Column
+	private String codigo;
+	@Column
+	private String url;
+	
+	
+	
+	public FuenteDeDatos() {
+		super();
+	}
+	
+	
+	public FuenteDeDatos(String codigo, String url) {
+		super();
+		this.codigo = codigo;
+		this.url = url;
+	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
