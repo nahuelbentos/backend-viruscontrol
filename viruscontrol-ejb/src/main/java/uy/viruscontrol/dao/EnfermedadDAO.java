52441ca6df63f61801d4e4a0ee369fef95916a4e
Natalie Elizabeth Di Bono Cabrera <natalie.di@fing.edu.uy>
2020-05-12 23:51:56 -0300

    Se creó package dao

---
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/dao/EnfermedadDAO.java
@@ -0,0 +1,23 @@
+package uy.viruscontrol.dao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class EnfermedadDAO
+ */
+@Stateless
+@LocalBean
+public class EnfermedadDAO implements EnfermedadDAOLocal {
+
+	
+	
+	
+    /**
+     * Default constructor. 
+     */
+    public EnfermedadDAO() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
