3950be2eeb53fd7377225fc8d1c591f18ded1742
Natalie Elizabeth Di Bono Cabrera <natalie.di@fing.edu.uy>
2020-05-13 17:46:21 -0300

    Enfermedad, Recurso, Sintoma, TipoEnfermedad

--- viruscontrol-ejb/src/main/java/uy/viruscontrol/entities/Enfermedad.java
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/entities/Enfermedad.java
@@ -13,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 
