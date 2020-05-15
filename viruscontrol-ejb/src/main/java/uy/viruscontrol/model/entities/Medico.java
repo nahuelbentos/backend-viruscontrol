534996f94092d20b0f64798d058e4b0b61209ab2
Jhonatan Sosa <jhonatansosaferreira@gmail.com>
2020-05-15 11:40:44 -0300

    Entity PrestadoraSalud
    * Se agrega entity PrestadoraSalud
    * Se agrega link de Medico a PrestadoraSalud
    * Se agrega link de Ciudadano a PrestadoraSalud

--- viruscontrol-ejb/src/main/java/uy/viruscontrol/model/entities/Medico.java
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/model/entities/Medico.java
@@ -2,14 +2,18 @@ package uy.viruscontrol.model.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 @DiscriminatorValue("medico")
 public class Medico extends Usuario implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private PrestadoraSalud prestadoraSalud;
 	
 	private boolean conectado;
 
---
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/model/entities/PrestadoraSalud.java
@@ -0,0 +1,47 @@
+package uy.viruscontrol.model.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "prestadora_salud")
+public class PrestadoraSalud implements Serializable {
+	private static final long serialVersionUID = -1223087316446990000L;
+	
+	@Id
+	private int id;
+	
+	@Column
+	private String nombre;
+	
+	public PrestadoraSalud() {
+		super();
+	}
+
+	public PrestadoraSalud(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
