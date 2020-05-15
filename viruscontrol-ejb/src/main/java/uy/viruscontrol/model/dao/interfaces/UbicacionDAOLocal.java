be511de7ab6026b6bd72063032ea87e8487cde2b
javier <javierms17@gmail.com>
2020-05-15 00:43:34 -0300

    DAOs ubicacion fuentededatos departamento y examen

---
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/model/dao/interfaces/UbicacionDAOLocal.java
@@ -0,0 +1,22 @@
+package uy.viruscontrol.model.dao.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import uy.viruscontrol.model.entities.Ubicacion;
+
+@Local
+public interface UbicacionDAOLocal {
+
+	void persist(Ubicacion ubicacion);
+
+	void merge(Ubicacion ubicacion);
+
+	List<Ubicacion> findAll();
+
+	Ubicacion findById(Integer id);
+
+	void delete(Ubicacion ubicacion);
+
+}
