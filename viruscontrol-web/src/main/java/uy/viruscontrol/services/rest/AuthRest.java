be1a4391235a52395ee99e1e93af7a2674b80eb3
maximiliano.farcilli <maximiliano.farcilli@fing.edu.uy>
2020-05-18 21:01:51 -0300

    Autenticacion desde Frontend funcionando.

--- viruscontrol-web/src/main/java/uy/viruscontrol/services/rest/AuthRest.java
+++ viruscontrol-web/src/main/java/uy/viruscontrol/services/rest/AuthRest.java
@@ -2,22 +2,35 @@ package uy.viruscontrol.services.rest;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import uy.viruscontrol.bussines.enumerated.AuthResponse;
 import uy.viruscontrol.bussines.enumerated.TipoUsuario;
 import uy.viruscontrol.bussines.interfaces.SessionBeanLocal;
-
-import uy.viruscontrol.model.entities.Usuario;
+import uy.viruscontrol.model.entities.Ciudadano;
+import uy.viruscontrol.model.entities.Medico;
 
 @ApplicationScoped
 public class AuthRest implements IAuthRest {
 
 	@EJB
 	private SessionBeanLocal sessionEjb;
-	
+
 	@Override
-	public AuthResponse iniciarSesion(Usuario user,TipoUsuario tipoUsuario) {
-		return sessionEjb.iniciarSesionConRedes(user, tipoUsuario);
+	public Response ping() {
+		return Response.status(Status.OK).build();
 	}
+
+	@Override
+	public AuthResponse iniciarSesionMedico(Medico user) {
+		return sessionEjb.iniciarSesionConRedes(user, TipoUsuario.MEDICO);
+	}
+
+	@Override
+	public AuthResponse iniciarSesionCiudadano(Ciudadano user) {
+		return sessionEjb.iniciarSesionConRedes(user, TipoUsuario.CIUDADANO);
+	}
+	
 
 }
--- viruscontrol-web/src/main/java/uy/viruscontrol/services/rest/IAuthRest.java
+++ viruscontrol-web/src/main/java/uy/viruscontrol/services/rest/IAuthRest.java
@@ -1,15 +1,17 @@
 package uy.viruscontrol.services.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import uy.viruscontrol.bussines.enumerated.AuthResponse;
-import uy.viruscontrol.bussines.enumerated.TipoUsuario;
-import uy.viruscontrol.model.entities.Usuario;
+
+import uy.viruscontrol.model.entities.Ciudadano;
+import uy.viruscontrol.model.entities.Medico;
 
 @Path("/autenticacion")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,8 +19,13 @@ import uy.viruscontrol.model.entities.Usuario;
 public interface IAuthRest{
 	
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/entrar/{tipoUsuario}")
-	public AuthResponse iniciarSesion(Usuario user, @PathParam("tipoUsuario") TipoUsuario tipoUsuario );
+	@Path("/entrar/medico")
+	public AuthResponse iniciarSesionMedico(Medico user);
+	@POST
+	@Path("/entrar/ciudadano")
+	public AuthResponse iniciarSesionCiudadano(Ciudadano user);
 	
+	@GET
+	@Path("/ping")
+	public Response ping();
 }
