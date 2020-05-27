a830c84d730217c83ccba362b9ca96c750de2063
maximiliano.farcilli <maximiliano.farcilli@fing.edu.uy>
2020-05-27 12:56:02 -0300

    Cambios en entidad visitamedico

--- viruscontrol-ejb/src/main/java/uy/viruscontrol/model/entities/VisitaMedico.java
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/model/entities/VisitaMedico.java
@@ -2,33 +2,64 @@ package uy.viruscontrol.model.entities;
 
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@IdClass(IdVisitaMedico.class)
-@Table(name = "visita_medico")
+@Table(name = "visita_medico",  uniqueConstraints={
+		   @UniqueConstraint(columnNames={"id_medico", "id_ciudadano", "fecha_asignacion"})
+		})
 public class VisitaMedico implements Serializable {
 	private static final long serialVersionUID = 771155363015049522L;
 
 	@Id
-	@Column(name = "id_medico")
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int idVisitaMedico;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_medico")
 	private Medico medico;
 	
-	@Id
-	@Column(name = "id_ciudadano")
+	@ManyToOne
+	@JoinColumn(name = "id_ciudadano")
 	private Ciudadano ciudadano;
 	
-	@Id
 	@Column(name = "fecha_asignacion")
 	private Calendar fechaAsignacion;
 
 	@Column(name = "visita_realizada")
 	private boolean visitaRealizada;
+
+	@ManyToMany(cascade = CascadeType.MERGE)
+	@JoinTable(name = "visitamedico_sintomas",
+		joinColumns = {
+			@JoinColumn(
+					name = "id_visitamedico",
+					referencedColumnName = "id"
+					)
+			},
+		inverseJoinColumns = {
+			@JoinColumn(
+					name = "id_sintoma",
+					referencedColumnName = "id"
+					)
+			}
+		)
+	private List<Sintoma> sintomas;
 	
 	public VisitaMedico() {
 		super();
