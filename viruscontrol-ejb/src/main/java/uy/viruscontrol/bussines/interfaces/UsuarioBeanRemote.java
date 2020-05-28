ebe21d22170e867c7305e2af1386cd7f9c34101a
javier <javierms17@gmail.com>
2020-05-28 00:45:51 -0300

    Lista y filtrado de usuarios BE y FE con jsf

--- viruscontrol-ejb/src/main/java/uy/viruscontrol/bussines/interfaces/UsuarioBeanRemote.java
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/bussines/interfaces/UsuarioBeanRemote.java
@@ -1,12 +1,26 @@
 package uy.viruscontrol.bussines.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 
+import uy.viruscontrol.model.entities.Administrador;
+import uy.viruscontrol.model.entities.Ciudadano;
+import uy.viruscontrol.model.entities.Gerente;
+import uy.viruscontrol.model.entities.Medico;
 import uy.viruscontrol.model.entities.Usuario;
 
 @Remote
 public interface UsuarioBeanRemote {
 	
 	public boolean registrarUsuario(Usuario user);
+	
+	List<Ciudadano> mostrarCiudadanos();
+	
+	List<Medico> mostrarMedicos();
+	
+	List<Gerente> mostrarGerentes();
+	
+	List<Administrador> mostrarAdministradores();
 	
 }
