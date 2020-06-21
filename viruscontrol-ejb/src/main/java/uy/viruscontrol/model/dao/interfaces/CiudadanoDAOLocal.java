c1c1195dc28a4617c9a53f9a8ad8330278c8e965
Jhonatan Sosa <jhonatansosaferreira@gmail.com>
2020-06-21 15:16:16 -0300

    Detección de posibles contagios
    * Se agrega thread con cálculo de posibles contagios según reporte de
    ubicaciones de usuarios y distancia configurada en enfermedades.
    * Se agrega notificación de casos en exposición al procesamiento de los
    casos.
    * Se habilitan notificaciones push desde nuestro proyecto.

--- viruscontrol-ejb/src/main/java/uy/viruscontrol/model/dao/interfaces/CiudadanoDAOLocal.java
+++ viruscontrol-ejb/src/main/java/uy/viruscontrol/model/dao/interfaces/CiudadanoDAOLocal.java
@@ -8,15 +8,9 @@ import uy.viruscontrol.model.entities.Ciudadano;
 
 @Local
 public interface CiudadanoDAOLocal {
-
 	void persist(Ciudadano ciudadano);
-
 	void merge(Ciudadano ciudadano);
-
 	List<Ciudadano> findAll();
-
 	Ciudadano findById(Integer id);
-
 	void delete(Ciudadano ciudadano);
-
 }
